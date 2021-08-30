eb268d8d78c038cdc9a646f0a48463faed53dad2
AlexandreC93 <alexandrecadinot93@gmail.com>
2021-08-30 15:02:32 +0200

    ok

--- src/components/Ship.jsx
+++ src/components/Ship.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import ImgShip from '../img/ship.png'
 
+const maxY = 1000
+const minY = 0
+const maxX = 1000
+const minX = 0
+
+
+
 class Ship extends React.Component {
-    state = {  }
+    
+
+
+
+   
     render() { 
         return ( 
 
             <div id="ship">
-            <img className="ship" src={ImgShip} alt="" />
+            <img className="ship" src={ImgShip} onKeyPress={this.props.key} alt="" />
             </div>
 
          );
--- src/components/Windows.jsx
+++ src/components/Windows.jsx
@@ -7,13 +7,41 @@ import Score from './Score'
 import Ship from './Ship'
 
 class Windows extends React.Component {
-    
-    state = {  }
+
+    constructor(props){
+        super(props)
+        this.state = { 
+            l: false,
+            r: false
+         }
+
+    }
+
+    setY = (y) => {
+        this.y = y
+    }
+
+    mooveKey = (e) =>{
+        if(e.keyC === "q"||"Q"){
+            this.setState({
+                l: true
+            })
+            console.log("q");
+        }
+
+        if(e.key === "d"||"D"){
+            this.setState({
+                r: true
+            })
+            console.log("d");
+        }
+        
+    }
     render() { 
         return ( 
 
             <div id="window">
-            <Ship/>
+            <Ship key={this.mooveKey}/>
             </div>
 
          );
