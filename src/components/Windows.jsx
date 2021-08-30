837df0c4d9821c478ba06d9ea4649ef8dd3e1071
AlexandreC93 <alexandrecadinot93@gmail.com>
2021-08-30 13:55:16 +0200

    depart

---
+++ src/components/Windows.jsx
@@ -0,0 +1,14 @@
+class Windows extends Component {
+    state = {  }
+    render() { 
+        return ( 
+
+            <>
+            {this.props.children}
+            </>
+
+         );
+    }
+}
+ 
+export default Windows;
