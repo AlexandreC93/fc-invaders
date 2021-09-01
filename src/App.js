7007f3e304748f19a65fe232fe16bc5719b776d5
ishranm9@gmail.com <ishranm9@gmail.com>
2021-09-01 10:47:13 +0200

    Add monster

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css"
 import Windows from './components/Windows'
 import Start from './components/Start'
 import Bullet from './components/Bullet'
-import Monster from './components/Monster'
+import Monster from './components/Monster.jsx'
 import Score from './components/Score'
 import Ship from './components/Ship'
 import Over from './components/over'
--- src/components/Monster.jsx
+++ src/components/Monster.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-
+import ImgMonster from '../img/Space.png'
 class Monster extends React.Component {
-    state = {  }
-    render() { 
+    constructor(props) {
+        super(props);
+        this.state = {}
+      }
+        render() { 
         return ( 
-
-            <>
-            </>
-
+             <img src={ImgMonster} alt="monster" className="monster" />
          );
     }
 }
- 
 export default Monster;
