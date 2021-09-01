970790efe080730d88fee4d8153ffd314944092d
AlexandreC93 <alexandrecadinot93@gmail.com>
2021-09-01 17:30:15 +0200

    fin mercredi

--- src/components/Bullet.jsx
+++ src/components/Bullet.jsx
@@ -10,8 +10,7 @@ class Bullet extends React.Component {
         return ( 
 
             <div id="bullet"
-            onKeyDown={(e)=>this.onKeyDown(e)}
-            tabIndex="0">
+           >
                 {}
             {/* <img className="bullet" src={ImgBullet} alt="" /> */}
             </div>
