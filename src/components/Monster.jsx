8bbf80e34bffe0946d451d962e943b4259f30c05
Mohamed-Amir kharmaz <amirpro@hotmail.fr>
2021-09-02 15:04:31 +0200

    okk

--- src/components/Monster.jsx
+++ src/components/Monster.jsx
@@ -4,22 +4,25 @@ import ImgMonster from '../img/Space.png'
 
 
 class Monster extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {}
   }
-  render() {
-    return (
-      <>
-      {
-        <img src={ImgMonster} alt="monster" className="monster" style={{gridColumn: this.props.mx, gridRow: this.props.my}} />
-      }
-      </>
 
-    );
+
+    render() {
+      return (
+        <>
+          {
+            <img src={ImgMonster} alt="monster" className="monster" style={{ gridColumn: this.props.mx, gridRow: this.props.my }} />
+          }
+        </>
+
+      );
+    }
   }
-}
+
+
+
 export default Monster;
-
-
-<i aria-hidden="true" class="optin monster big icon"></i>
